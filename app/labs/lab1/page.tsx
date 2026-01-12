0b883158cb8de0ce4b07736e61a51aea2684d440
Kenneth Aldridge <kenneth.r.aldridge@gmail.com>
2026-01-12 17:32:41 -0500

    finished lab

--- app/labs/lab1/page.tsx
+++ app/labs/lab1/page.tsx
@@ -31,15 +31,230 @@ export default function Lab1() {
           <h4>List Tags</h4>
           <h5>Ordered List Tag</h5>
           How to make pancakes:
-          1. Mix dry ingredients.
-          2. Add wet ingredients.
-          3. Stir to combine.
-          4. Heat a skillet or griddle.
-          5. Pour batter onto the skillet.
-          6. Cook until bubbly on top.
-          7. Flip and cook the other side.
-          8. Serve and enjoy!
+          <ol id="wd-pancakes">
+            <li>Mix dry ingredients.</li>
+            <li>Add wet ingredients.</li>
+            <li>Stir to combine.</li>
+            <li>Heat a skillet or griddle.</li>
+            <li>Pour batter onto the skillet.</li>
+            <li>Cook until bubbly on top.</li>
+            <li>Flip and cook the other side.</li>
+            <li>Serve and enjoy!</li>
+          </ol>
+          Chicken parm recipe:
+          <ol id="wd-your-favorite-recipe">
+            <li>Prepare chicken by seasoning then covering with breadcrumbs.</li>
+            <li>Fry chicken for around 8 minutes.</li>
+            <li>Bake chicken with parmesan and marinara sauce</li>
+            <li>Finish by adding extra seasoning.</li>
+            <li>Serve and enjoy!</li>
+          </ol>
+          <h5>Unordered List Tag</h5>
+          My favorite books (in no particular order)
+          <ul id="wd-my-books">
+            <li>Dune</li>
+            <li>Lord of the Rings</li>
+            <li>Enders Game</li>
+            <li>Red Mars</li>
+            <li>The Forever War</li>
+          </ul>
+          Your favorite books (in no particular order)
+          <ul id="wd-your-books">
+            <li>Harry Potter</li>
+            <li>Moneyball</li>
+            <li>Fever Pitch</li>
+            <li>Unbroken</li>
+          </ul>
         </div>
+
+        <div id="wd-tables">
+          <h4>Table Tag</h4>
+          <table border={1} width="100%">
+            <thead>
+              <tr>
+                <th>Quiz</th>
+                <th>Topic</th>
+                <th>Date</th>
+                <th>Grade</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Q1</td>
+                <td>HTML</td>
+                <td>2/3/21</td>
+                <td>85</td>
+              </tr>
+              <tr>
+                <td>Q2</td>
+                <td>CSS</td>
+                <td>2/10/21</td>
+                <td>90</td>
+              </tr>
+              <tr> 
+                <td>Q3</td>
+                <td>CSS</td>
+                <td>2/17/21</td>
+                <td>95</td>
+              </tr>
+              <tr> 
+                <td>Q4</td>
+                <td>JS</td>
+                <td>2/21/21</td>
+                <td>83</td>
+              </tr>
+              <tr> 
+                <td>Q5</td>
+                <td>JS</td>
+                <td>2/25/21</td>
+                <td>19</td>
+              </tr>
+              <tr> 
+                <td>Q6</td>
+                <td>State</td>
+                <td>3/1/21</td>
+                <td>78</td>
+              </tr>
+              <tr> 
+                <td>Q7</td>
+                <td>Redux</td>
+                <td>3/7/21</td>
+                <td>99</td>
+              </tr>
+              <tr> 
+                <td>Q8</td>
+                <td>Node</td>
+                <td>3/15/21</td>
+                <td>91</td>
+              </tr>
+              <tr> 
+                <td>Q9</td>
+                <td>Node</td>
+                <td>3/25/21</td>
+                <td>97</td>
+              </tr>
+              <tr> 
+                <td>Q10</td>
+                <td>Mongo</td>
+                <td>4/2/21</td>
+                <td>89</td>
+              </tr>
+            </tbody>
+            <tfoot>
+              <tr>
+                <td colSpan={3}>Average</td>
+                <td>90</td>
+              </tr>
+            </tfoot>
+          </table>
+        </div>
+
+        <div id="wd-images">
+          <h4>Image tag</h4>
+          Loading an image from the internet: <br />
+          <img id="wd-starship" width="400px"   src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg" />
+          <br />
+          Loading a local image:
+          <br />
+          <img id="wd-teslabot" src="/images/teslabot.jpg" height="200px" />
+        </div>
+
+        <div id="wd-forms">
+          <h4>Form Elements</h4>
+          <form id="wd-text-fields">
+            <h5>Text Fields</h5>
+            <label htmlFor="wd-text-fields-username">Username:</label>
+            <input placeholder="jdoe" id="wd-text-fields-username" /> <br />
+            <label htmlFor="wd-text-fields-password">Password:</label>
+            <input type="password" defaultValue="123@#$asd"
+                  id="wd-text-fields-password" /><br />
+            <label htmlFor="wd-text-fields-first-name">First name:</label>
+            <input type="text" title="John" id="wd-text-fields-first-name" /> <br />
+            <label htmlFor="wd-text-fields-last-name">Last name:</label>
+            <input type="text" placeholder="Doe"
+                  defaultValue="Wonderland"
+                  title="The last name"
+                  id="wd-text-fields-last-name" />
+            <h5>Text boxes</h5>
+              <label>Biography:</label><br/>
+              <textarea id="wd-textarea" cols={30} rows={10}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</textarea>
+            <h5 id="wd-radio-buttons">Radio buttons</h5>
+              <label>Favorite movie genre:</label><br />
+
+              <input type="radio" name="radio-genre" id="wd-radio-comedy"/>
+              <label htmlFor="wd-radio-comedy">Comedy</label><br />
+
+              <input type="radio" name="radio-genre" id="wd-radio-drama"/>
+              <label htmlFor="wd-radio-drama">Drama</label><br />
+
+              <input type="radio" name="radio-genre" id="wd-radio-scifi"/>
+              <label htmlFor="wd-radio-scifi">Science Fiction</label><br />
+              <input type="radio" name="radio-genre" id="wd-radio-fantasy"/>
+              <label htmlFor="wd-radio-fantasy">Fantasy</label>  
+            <h5 id="wd-checkboxes">Checkboxes</h5>
+              <label>Favorite movie genre:</label><br/>
+
+              <input type="checkbox" name="check-genre" id="wd-chkbox-comedy"/>
+              <label htmlFor="wd-chkbox-comedy">Comedy</label><br/>
+
+              <input type="checkbox" name="check-genre" id="wd-chkbox-drama"/>
+              <label htmlFor="wd-chkbox-drama">Drama</label><br/>
+
+              <input type="checkbox" name="check-genre" id="wd-chkbox-scifi"/>
+              <label htmlFor="wd-chkbox-scifi">Science Fiction</label><br/>
+
+              <input type="checkbox" name="check-genre" id="wd-chkbox-fantasy"/>
+              <label htmlFor="wd-chkbox-fantasy">Fantasy</label>
+            <h4 id="wd-dropdowns">Dropdowns</h4>
+              <h5>Select one</h5>
+              <label  htmlFor="wd-select-one-genre"> Favorite movie genre: </label><br/>
+              <select id="wd-select-one-genre">
+                <option value="COMEDY">Comedy</option>
+                <option value="DRAMA">Drama</option>
+                <option selected value="SCIFI">
+                    Science Fiction</option>
+                <option value="FANTASY">Fantasy</option>
+              </select>
+              <h5>Select many</h5>
+              <label  htmlFor="wd-select-many-genre"> Favorite movie genres: </label><br/>
+              <select multiple id="wd-select-many-genre">
+                <option value="COMEDY" selected> Comedy          </option>
+                <option value="DRAMA">           Drama           </option>
+                <option value="SCIFI"  selected> Science Fiction </option>
+                <option value="FANTASY">         Fantasy         </option>
+              </select>
+            <h4>Other HTML field types</h4>
+              <label htmlFor="wd-text-fields-email"> Email: </label>
+              <input type="email"
+                    placeholder="jdoe@somewhere.com"
+                    id="wd-text-fields-email"/><br/>
+
+              <label htmlFor="wd-text-fields-salary-start"> Starting salary:</label>
+              <input type="number"
+                    defaultValue="100000"
+                    placeholder="1000"
+                    id="wd-text-fields-salary-start"/><br/>
+
+              <label htmlFor="wd-text-fields-rating"> Rating: </label>
+              <input type="range"
+                    defaultValue="4"
+                    max="5"
+                    placeholder="Doe"
+                    id="wd-text-fields-rating"/><br/>
+
+              <label htmlFor="wd-text-fields-dob"> Date of birth: </label>
+              <input type="date"
+                    defaultValue="2000-01-21"
+                    id="wd-text-fields-dob"/><br/>
+          </form>
+        </div>
+
+        <div>
+          <h4>Anchor tag</h4>
+            Please <a href="https://www.lipsum.com" id="wd-lipsum">click here</a> to get dummy text<br/>
+            Please <a href="https://github.com/kennethaldridge/kambaz-next-js-sp26" id="wd-github">click here</a> to get to github<br/>
+        </div>
+       
       </div>
     );
   }
