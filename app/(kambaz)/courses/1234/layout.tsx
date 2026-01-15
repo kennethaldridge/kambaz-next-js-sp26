4638a84013ef029fb080056829292bb873280255
Kenneth Aldridge <kenneth.r.aldridge@gmail.com>
2026-01-14 23:01:56 -0500

    more fix

--- app/(kambaz)/courses/1234/layout.tsx
+++ app/(kambaz)/courses/1234/layout.tsx
@@ -1,10 +1,11 @@
 import { ReactNode } from "react";
 import CourseNavigation from "./navigation";
-
- export default function CoursesLayout({ children }: { children: ReactNode }){
+export default async function CoursesLayout(
+  { children, params }: Readonly<{ children: ReactNode; params: Promise<{ id: string }> }>) {
+ const { cid } = await params;
  return (
    <div id="wd-courses">
-     <h2>Courses</h2>
+     <h2>Courses {cid}</h2>
      <hr />
      <table>
        <tbody>
@@ -16,3 +17,4 @@ import CourseNavigation from "./navigation";
      </table>
    </div>
 );}
+
