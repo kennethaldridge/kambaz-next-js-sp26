d1bd3d3e1df178deb721cc8d11c2964891e6d586
Kenneth Aldridge <kenneth.r.aldridge@gmail.com>
2026-02-05 17:59:39 -0500

    starting a3

--- app/labs/lab3/page.tsx
+++ app/labs/lab3/page.tsx
@@ -1,7 +1,51 @@
+import VariablesAndConstants from "./VariablesAndConstants";
+import VariableTypes from "./VariableTypes";
+import BooleanVariables from "./BooleanVariables";
+import IfElse from "./IfElse";
+import TernaryOperator from "./TernaryOperator";
+import ConditionalOutputIfElse from "./ConditionalOutputIfElse";
+import ConditionalOutputInline from "./ConditionalOutputInLine";
+import LegacyFunctions from "./LegacyFunctions";
+import ArrowFunctions from "./ArrowFunctions";
+import ImpliedReturn from "./ImpliedReturn";
+import TemplateLiterals from "./TemplateLiterals";
+import SimpleArrays from "./SimpleArrays";
+import ArrayIndexAndLength from "./ArrayIndexAndLength";
+import AddingAndRemovingToFromArrays from "./AddingAndRemovingToFromArrays";
+import ForLoops from "./ForLoops";
+import MapFunction from "./MapFunction";
+import FindFunction from "./FindFunction";
+import FindIndex from "./FindIndex";
+import FilterFunction from "./FilterFunction";
+
 export default function Lab3() {
-    return (
+  return(
+    <div id="wd-lab3">
+      <h3>Lab 3</h3>
       <div>
-        <h2>Lab 3</h2>
+        <VariablesAndConstants/>
+        <VariableTypes/>
+        <BooleanVariables/>
+        <IfElse/>
+        <TernaryOperator/>
+        <ConditionalOutputIfElse/>
+        <ConditionalOutputInline/>
+        <LegacyFunctions/>
+        <ArrowFunctions/>
+        <ImpliedReturn/>
+        <TemplateLiterals/>
+        <SimpleArrays/>
+        <ArrayIndexAndLength/>
+        <AddingAndRemovingToFromArrays/>
+        <ForLoops/>
+        <MapFunction/>
+        <FindFunction/>
+        <FindIndex/>
+        <FilterFunction/>
       </div>
-  );}
+    </div>
+  );
+}
+
+
   
