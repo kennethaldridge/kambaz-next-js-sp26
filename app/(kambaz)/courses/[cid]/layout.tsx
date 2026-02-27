dbb3c9f0cb110abf791049bd11f0bfdd25f789c6
Kenneth Aldridge <kenneth.r.aldridge@gmail.com>
2026-02-26 19:03:00 -0500

    fixing error cid

--- app/(kambaz)/courses/[cid]/layout.tsx
+++ app/(kambaz)/courses/[cid]/layout.tsx
@@ -10,26 +10,24 @@ import { FaAlignJustify } from "react-icons/fa";
 export default function CoursesLayout({ children }: { children: ReactNode }) {
   const router = useRouter();
   const { cid } = useParams<{ cid: string }>();
-
   const { courses } = useSelector((state: RootState) => state.coursesReducer);
-  const { currentUser } = useSelector((state: RootState) => state.accountReducer);
+  const { currentUser } =
+    useSelector((state: RootState) => state.accountReducer) as any;
   const { enrollments } = useSelector(
     (state: RootState) => state.enrollmentsReducer
   );
   const course = courses.find((course: any) => course._id === cid);
   const [showCourseNav, setShowCourseNav] = useState(true);
+  const userId = currentUser?._id;
   const enrolled =
-    !!currentUser?._id &&
-    enrollments.some(
-      (e: any) => e.user === currentUser._id && e.course === cid
-    );
+    !!userId &&
+    enrollments.some((e: any) => e.user === userId && e.course === cid);
   useEffect(() => {
     if (!enrolled) {
       router.push("/dashboard");
     }
   }, [enrolled, router]);
   if (!enrolled) return null;
-
   return (
     <div id="wd-courses">
       <h2>
